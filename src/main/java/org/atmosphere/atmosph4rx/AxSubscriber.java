2a33b792e5d492a32921f0d2fa1bac7331c0606a
jfarcand <jfarcand@apache.org>
2018-05-24 17:22:31 -0400

    Start adding javadoc

--- src/main/java/org/atmosphere/atmosph4rx/AxSubscriber.java
+++ src/main/java/org/atmosphere/atmosph4rx/AxSubscriber.java
@@ -16,41 +16,94 @@
 package org.atmosphere.atmosph4rx;
 
 import org.reactivestreams.Processor;
+import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+/**
+ * An extended {@link Subscriber} with support extended functionality.
+ *
+ * @param <T>
+ */
 public interface AxSubscriber<T> extends Subscriber<T> {
 
+    /**
+      * {@inheritDoc}
+      */
     @Override
     default void onNext(T payload) {
     }
 
+    /**
+      * {@inheritDoc}
+      */
     @Override
     default void onSubscribe(Subscription s) {
         s.request(Integer.MAX_VALUE);
     }
 
+    /**
+      * {@inheritDoc}
+      */
     @Override
     default void onComplete() {
     }
 
+    /**
+      * {@inheritDoc}
+      */
     @Override
     default void onError(Throwable t) {
     }
 
-    default void onSubscribe(AxSubscription s) {
+    /**
+     * Same behavior as {@link #onSubscribe(Subscription)}, but with an extended version of {@link Subscription} called {@link AxSubscription}.
+     *
+     * @param subscription the element signaled
+     */
+    default void onSubscribe(AxSubscription subscription) {
     }
 
+    /**
+     * Data notification sent by a {@link Publisher} in response to requests to {@link Subscription#request(long)}.
+     * The {@code payload} can comes from a {@link MultiLink}, a {@link Link} or a websocket/http connection.
+     *
+     * @param payload the element signaled
+     */
     default void onNext(byte[] payload) {
     }
 
-    default <U extends Processor<? super String, ? super String>> void onNext(Link<U, String> single, String payload) {
+    /**
+     * Data notification sent by a {@link Publisher} in response to requests to {@link Subscription#request(long)}.
+     * The {@code payload} can comes from a {@link MultiLink}, a {@link Link} or a websocket/http connection.
+     *
+     * @param link The {@link Link} representing the underlying websocket or http connection
+     * @param payload The {@link String} payload received
+     * @param <U>
+     */
+    default <U extends Processor<? super String, ? super String>> void onNext(Link<U, String> link, String payload) {
     }
 
-    default <U extends Processor<? super String, ? super String>, V> void onNext(Link<U, V> single) {
+    /**
+     * Data notification sent by the {@link Publisher} in response to requests to {@link Subscription#request(long)}.
+     * The {@code payload} can comes from a {@link MultiLink}, a {@link Link} or a websocket/http connection.
+     *
+     * @param link
+     * @param <U>
+     * @param <V>
+     */
+    default <U extends Processor<? super String, ? super String>, V> void onNext(Link<U, V> link) {
     }
 
-    default <U extends Processor<? super String, ? super String>> void onNext(Link<U, byte[]> single, byte[] payload) {
+    /**
+     * Data notification sent by a {@link Publisher} in response to requests to {@link Subscription#request(long)}.
+     * The {@code payload} can comes from a {@link MultiLink}, a {@link Link} or a websocket/http connection.
+     *
+     * @param link The {@link Link} representing the underlying websocket or http connection
+     * @param payload The {@link byte} payload received
+     * @param <U>
+     */
+    default <U extends Processor<? super String, ? super String>> void onNext(Link<U, byte[]> link, byte[] payload) {
     }
 
 }
